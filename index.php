e07e24ab360253a1265f442d2740e9a5d1469730
Ian Cox <ian@iancox.com>
2014-07-08 01:46:45 -0400

    +local version of jquery for offline dev

--- index.php
+++ index.php
@@ -5,9 +5,13 @@ require_once('config.php');
 
 <html>
 <head>
-  <link rel="stylesheet" type="text/css" href="assets/css/screen.css">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  	<link rel="stylesheet" type="text/css" href="assets/css/screen.css">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  	<!-- Google CDN -->
+  	<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> -->
+
+  	<!-- FOR LOCAL DEV -->
+	<script src="assets/js/jquery.min.js"></script>
 </head>
 
 <body id="fileviewer">
@@ -16,6 +20,6 @@ require_once('config.php');
 		<?php include('includes/directory.php') ?>
 	</div>
 
-  <script src="assets/js/custom.js"></script>
+  	<script src="assets/js/custom.js"></script>
 </body>
 </html>
